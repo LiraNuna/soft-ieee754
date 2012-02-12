817810633973ac530637b16070f98f2e56af6fe4
Liran Nuna <liranuna@gmail.com>
2012-02-12 13:46:10 -0800

    Added untested compatibility for MSVC

--- includes/smallest_unsigned.hpp
+++ includes/smallest_unsigned.hpp
@@ -1,7 +1,19 @@
 #ifndef __SMALLEST_UNSIGNED_H__
 #define __SMALLEST_UNSIGNED_H__
 
+#if defined(_MSC_VER) && _MSC_VER < 1600
+typedef __int8 int8_t;
+typedef __int16 int16_t;
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+#else
 #include <stdint.h>
+#endif
 
 template<unsigned bits >
 struct smallest_unsigned {
