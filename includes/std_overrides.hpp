ef491c759e8f89d8d35fa3563121b0cbb7aa5b54
Liran Nuna <liranuna@gmail.com>
2012-12-26 14:21:20 -0800

    Change all const expression to use the constexpr keyword

--- includes/std_overrides.hpp
+++ includes/std_overrides.hpp
@@ -14,47 +14,49 @@ namespace std {
 	template <unsigned M, unsigned E, int B >
 	struct numeric_limits<IEEE754<M, E, B > > {
 		public:
-			static const bool is_specialized = true;
+			static constexpr bool is_specialized = true;
 
 			static IEEE754<M, E, B > min()			{ return from_components(+1, +1); }
 			static IEEE754<M, E, B > max()			{ return from_components(-2, -2); }
 
-			static const int digits = M + 1;
-			static const int digits10 = M * M_LOG10E / M_LOG2E;
-			static const bool is_signed = true;
-			static const bool is_integer = false;
-			static const bool is_exact = false;
-			static const int radix = 2;
+			static constexpr int digits = M + 1;
+			static constexpr int digits10 = M * M_LOG10E / M_LOG2E;
+			static constexpr bool is_signed = true;
+			static constexpr bool is_integer = false;
+			static constexpr bool is_exact = false;
+			static constexpr int radix = 2;
 
 			static IEEE754<M, E, B > epsilon()		{ return from_components(B - M, 0); }
 			static IEEE754<M, E, B > round_error()	{ return 0.5f; }
 
-			static const int min_exponent = -B + 2;
-			static const int max_exponent =  B + 1;
-			static const int min_exponent10 = min_exponent * M_LOG10E / M_LOG2E;
-			static const int max_exponent10 = max_exponent * M_LOG10E / M_LOG2E;
+			static constexpr int min_exponent = -B + 2;
+			static constexpr int max_exponent =  B + 1;
+			static constexpr int min_exponent10 = min_exponent * M_LOG10E / M_LOG2E;
+			static constexpr int max_exponent10 = max_exponent * M_LOG10E / M_LOG2E;
 
-			static const bool has_infinity = true;
-			static const bool has_quiet_NaN = true;
-			static const bool has_signaling_NaN = false;
-			static const float_denorm_style has_denorm = denorm_present;
-			static const bool has_denorm_loss = false;
+			static constexpr bool has_infinity = true;
+			static constexpr bool has_quiet_NaN = true;
+			static constexpr bool has_signaling_NaN = false;
+			static constexpr float_denorm_style has_denorm = denorm_present;
+			static constexpr bool has_denorm_loss = false;
 
 			static IEEE754<M, E, B > infinity()		{ return from_components(-1,  0); }
 			static IEEE754<M, E, B > quiet_NaN()	{ return from_components(-1, +1); }
 			static IEEE754<M, E, B > signaling_NaN(){ return from_components(-1, +1); }
 			static IEEE754<M, E, B > denorm_min()	{ return from_components( 0, +1); }
 
-			static const bool is_iec559 = has_infinity && has_quiet_NaN && has_denorm == denorm_present;
-			static const bool is_bounded = true;
-			static const bool is_modulo = false;
+			static constexpr bool is_iec559 = has_infinity && has_quiet_NaN && has_denorm == denorm_present;
+			static constexpr bool is_bounded = true;
+			static constexpr bool is_modulo = false;
 
-			static const bool traps = false;
-			static const bool tinyness_before = false;
-			static const float_round_style round_style = round_to_nearest;
+			static constexpr bool traps = false;
+			static constexpr bool tinyness_before = false;
+			static constexpr float_round_style round_style = round_to_nearest;
 
 		private:
-			inline static IEEE754<M, E, B > from_components(typename IEEE754<M, E, B >::primitive exponent, typename IEEE754<M, E, B >::primitive mantissa) {
+			typedef typename IEEE754<M, E, B >::primitive primitive;
+
+			inline static IEEE754<M, E, B > from_components(primitive exponent, primitive mantissa) {
 				IEEE754<M, E, B > result;
 				result.mantissa	= mantissa;
 				result.exponent	= exponent;
